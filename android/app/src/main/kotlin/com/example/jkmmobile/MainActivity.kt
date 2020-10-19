53016a24bf5968b602c07b223adbada00f0a107d
James Kituku <cit2230162015@mmu.ac.ke>
2020-10-20 00:12:05 +0300

    add files

---
+++ android/app/src/main/kotlin/com/example/jkmmobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jkmmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: WebViewLoad()));
+  }
+}
+
+class WebViewLoad extends StatefulWidget {
+  WebViewLoadUI createState() => WebViewLoadUI();
+}
+
+class WebViewLoadUI extends State<WebViewLoad> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // backgroundColor: Color(0xff885566),
+          title: new Text(
+            "Jumia Kilimall Masoko",
+            textAlign: TextAlign.center,
+            style: new TextStyle(color: Colors.white),
+          ),
+          leading: new IconButton(
+            icon: new Icon(Icons.home_rounded, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyApp()),
+              );
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+              onPressed: () => exit(0),
+            ),
+          ],
+        ),
+        body: SafeArea(
+            child: WebView(
+          initialUrl: 'https://jkm-bot.herokuapp.com',
+          javascriptMode: JavascriptMode.unrestricted,
+        )));
+  }
+}
